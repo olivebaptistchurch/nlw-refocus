004be7390f77525cb36e70641a1172398c460f2b
Colton Plumley <cplumley@olivebaptist.org>
2021-02-10 09:57:19 -0600

    Fix breakout links on tablet

--- src/components/molecules/BreakoutLink/BreakoutLinks.style.js
+++ src/components/molecules/BreakoutLink/BreakoutLinks.style.js
@@ -1,12 +1,23 @@
 import styled from "@emotion/styled"
+import { theme } from "../../utilities/Theme"
 
 const StyledBreakoutLinks = styled.div`
   margin-bottom: 1.5rem;
 
-  grid-column: ${props =>
+
+  @media screen and (max-width: ${theme.screens.tablet}) {
+    grid-column: ${props =>
       props.centeredMobile
         ? `1/3`
         : null};
+  }
+
+  @media screen and (max-width: ${theme.screens.mobile}) {
+    grid-column: ${props =>
+      props.centeredMobile
+        ? `unset`
+        : null};
+  }
 
   p {
     margin: .75rem;
